e5061f7ffc73eb77d54febc76a0ebc6a613bd523
Heather Romney <heatherromney@gmail.com>
2016-12-28 00:33:56 -0700

    Added Data Access Object pattern.

---
+++ src/main/java/com/iag1/compositeEntity/CompositeEntityPatternDemo.java
@@ -0,0 +1,33 @@
+package com.iag1.compositeEntity;
+
+/**
+ * Composite Entity pattern is used in EJB persistence mechanism. A Composite entity is an EJB entity bean which
+ * represents a graph of objects. When a composite entity is updated, internally dependent objects beans get updated
+ * automatically as being managed by EJB entity bean. Following are the participants in Composite Entity Bean.
+ * <p>
+ * Composite Entity - It is primary entity bean. It can be coarse grained or can contain a coarse grained object to
+ * be used for persistence purpose.
+ * <p>
+ * Coarse-Grained Object - This object contains dependent objects. It has its own life cycle and also manages life
+ * cycle of dependent objects.
+ * <p>
+ * Dependent Object - Dependent object is an object which depends on coarse grained object for its persistence
+ * lifecycle.
+ * <p>
+ * Strategies - Strategies represents how to implement a Composite Entity.
+ * <p>
+ * Implementation
+ * We are going to create CompositeEntity object acting as CompositeEntity. CoarseGrainedObject will be a class
+ * which contains dependent objects. CompositeEntityPatternDemo, our demo class will use Client class to demonstrate
+ * use of Composite Entity pattern.
+ */
+public class CompositeEntityPatternDemo {
+    public static void main(String[] args) {
+
+        Client client = new Client();
+        client.setData("Test", "Data");
+        client.printData();
+        client.setData("Second Test", "Data1");
+        client.printData();
+    }
+}
