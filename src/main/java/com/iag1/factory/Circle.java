8804f8bebda7015d6bf423c1bb77603da26c1829
Heather Romney <heatherromney@gmail.com>
2016-12-27 17:23:33 -0700

    Added Factory pattern.

---
+++ src/main/java/com/iag1/factory/Circle.java
@@ -0,0 +1,8 @@
+package com.iag1.factory;
+
+public class Circle implements Shape {
+
+   public void draw() {
+      System.out.println("Inside Circle::draw() method.");
+   }
+}
---
+++ src/main/java/com/iag1/factory/Square.java
@@ -0,0 +1,8 @@
+package com.iag1.factory;
+
+public class Square implements Shape {
+
+   public void draw() {
+      System.out.println("Inside Square::draw() method.");
+   }
+}
